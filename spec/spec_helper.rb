b52021f02d5f3c41638acbf0da9bea1af313c140
Ryan Brooks <ryan@slatehorse.com>
2019-02-13 16:15:22 +0000

    rubocopping

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,8 +42,8 @@ RSpec.configure do |config|
 
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options.
-  config.example_status_persistence_file_path = "spec/.rspec_state.txt"
- 
+  config.example_status_persistence_file_path = 'spec/.rspec_state.txt'
+
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
@@ -58,9 +58,9 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
+    config.default_formatter = 'doc'
   end
-  
+
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
