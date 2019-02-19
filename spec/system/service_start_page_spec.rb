559e4b80b2a3881059446c304f0ae44c28387a7f
Ryan Brooks <ryan@slatehorse.com>
2019-02-19 17:01:29 +0000

    Update service start page tests (TODO)

--- spec/system/service_start_page_spec.rb
+++ spec/system/service_start_page_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe 'Service start page', type: :system do
     visit '/'
     expect(page).to have_text('Find and explore data in the National Pupil Database')
     expect(page).to have_link('Start now')
+
+    # TODO: add tests for external links
+    #expect(page).to have_link('An introduction to the National Pupil Database', TODO)
+    #expect(page).to have_link('Apply for National Pupil Database data', TODO)
+    #expect(page).to have_link('Become an ONS accredited researcher', TODO)
+    #expect(page).to have_link('Register to become an approved research centre', TODO)
+
+    #expect(page).to have_link('Secure Research Service', TODO)
+
+    #expect(page).to have_link('2018 version of the data table', TODO)
+	expect(page).to have_link('data.sharing@education.gov.uk', href: 'mailto:data.sharing@education.gov.uk')
+  end
+
+  xit 'Has search' do
   end
 end
