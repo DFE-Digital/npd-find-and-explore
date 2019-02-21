bb070ec7532e14ea52e3a058585ef4fe49aa962d
Ryan Brooks <ryan@slatehorse.com>
2019-02-21 08:31:32 +0000

    WIP docker improvements for webpacker

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -30,6 +30,7 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
+    policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'http://0.0.0.0:3035', 'ws://0.0.0.0:3035'
   else
     policy.script_src :self, :https
   end
