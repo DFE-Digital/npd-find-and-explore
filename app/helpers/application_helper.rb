0960b59dae5f28b5669a90dff52c2246bb150e3a
cacquani <cacquani@mail.com>
2019-03-25 11:01:11 +0000

    Detail page - various
    Issue #32: data item name
    Issue #33: previous data item name
    Issue #35: date data item collected from and to
    Issue #34: term data item collected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  ACADEMIC_TERMS = {
+    aut: 'Autumn',
+    spr: 'Spring',
+    sum: 'Summer'
+  }.freeze
+
+  def academic_year(year)
+    return 'present' if year.blank?
+
+    "#{year} - #{(year + 1).to_s[2, 2]}"
+  end
+
+  def academic_term(term)
+    ACADEMIC_TERMS[term&.downcase&.to_sym] || term
+  end
 end
