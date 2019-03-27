a6948ba1463b1f5360d0aa0b00e6efc415977bba
cacquani <cacquani@mail.com>
2019-03-27 10:44:38 +0000

    Issue #48: Rails admin behind login
    * Put Rails Admin behind login
    * Make devise routes only available in development environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users
+  devise_for :admin_users if Rails.env.development?
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories, only: %i[index show]
