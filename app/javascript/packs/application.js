b39e5a1947f49b96067c2fd1b98a7571c2376d95
cacquani <cacquani@mail.com>
2019-04-08 11:27:30 +0100

    #76 rearrange JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,14 +9,4 @@
 
 import '../src/application.scss'
 
-import $ from 'jquery'
-
 import '../src/govuk-frontend.js'
-import '../src/sort.js'
-import '../src/step-by-step-navigation.js'
-
-$(document).ready(function() {
-  var $element = $('#step-by-step-navigation')
-  var stepByStepNavigation = new GOVUK.Modules.StepByStepNavigation()
-  stepByStepNavigation.start($element)
-})
