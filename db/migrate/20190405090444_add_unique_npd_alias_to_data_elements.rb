b5fc5a91b5fe073be7a67772cef95a3fe3fe8c17
cacquani <cacquani@mail.com>
2019-04-08 13:50:49 +0100

    #80 fix NPD alias migration

--- db/migrate/20190405090444_add_unique_npd_alias_to_data_elements.rb
+++ db/migrate/20190405090444_add_unique_npd_alias_to_data_elements.rb
@@ -4,7 +4,7 @@ class AddUniqueNpdAliasToDataElements < ActiveRecord::Migration[5.2]
   def up
     add_column :data_elements, :npd_alias, :string
     execute <<~'SQL'
-      UPDATE data_elements SET npd_alias = additional_attributes->'npd_alias'->0
+      UPDATE data_elements SET npd_alias = regexp_replace(regexp_replace(additional_attributes->'npd_alias'->>0, '^ +', ''), ' +$', '')
     SQL
     change_column_null :data_elements, :npd_alias, false, ''
     add_index :data_elements, :npd_alias, unique: true
