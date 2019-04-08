b4c56d87374d69abdc04b1d509067171e446cac2
cacquani <cacquani@mail.com>
2019-04-08 14:18:07 +0100

    #76 fixes after review and JS linting

--- app/javascript/packs/navigation.js
+++ app/javascript/packs/navigation.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 import '../src/govuk-frontend.js'
 import '../src/step-by-step-navigation.js'
 
-$(document).ready(function() {
+$(document).ready(function () {
   var $element = $('#step-by-step-navigation')
   var stepByStepNavigation = new GOVUK.Modules.StepByStepNavigation()
   stepByStepNavigation.start($element)
