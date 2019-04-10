89de33821ca4ea056436ec8700b2a575c1d6d543
Ryan Brooks <ryan@slatehorse.com>
2019-04-10 11:39:00 +0100

    Remove Data Element translations from migrations because the gem breaks migrations when translations aren't consistent with the current model file

--- db/migrate/20190404084522_move_translations_into_data_elements.rb
+++ db/migrate/20190404084522_move_translations_into_data_elements.rb
@@ -5,11 +5,9 @@ class MoveTranslationsIntoDataElements < ActiveRecord::Migration[5.2]
     add_column :data_elements, :description_en, :text
     add_column :data_elements, :description_cy, :text
 
-    drop_table :data_element_translations
   end
 
   def down
-    DataElement.create_translation_table! description: :text
 
     remove_column :data_elements, :description_en, :text
     remove_column :data_elements, :description_cy, :text
