1d523dede2ea02b57b7d2e3ff1d36342c5f97f16
Ryan Brooks <ryan@slatehorse.com>
2019-04-10 13:01:32 +0100

    Explicitly declare the fuzzystrmatch extension, to prevent failure if structure is loaded before schema.rb

--- db/structure.sql
+++ db/structure.sql
@@ -1,4 +1,5 @@
 -- multi-search support added in 20190328115623_create_pg_search_documents.rb
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 CREATE OR REPLACE FUNCTION pg_search_dmetaphone(text) RETURNS text LANGUAGE SQL IMMUTABLE STRICT AS $function$
 SELECT array_to_string(ARRAY(SELECT dmetaphone(unnest(regexp_split_to_array($1, E'\\s+')))), ' ')
 $function$;
