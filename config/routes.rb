1e6a57486fb36ea1d861cf990624ebced8acc945
cacquani <cacquani@mail.com>
2019-04-24 13:42:55 +0100

    #119 enable admin routes in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users if Rails.env.development?
+  devise_for :admin_users unless Rails.env.test?
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
