33c55ed9a6d2d05d822978a7af5a8308a3166057
cacquani <cacquani@mail.com>
2019-04-25 16:44:42 +0100

    #120 basic administrate setup

---
+++ app/dashboards/data_element_dashboard.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
+
+class DataElementDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    versions: Field::HasMany.with_options(class_name: 'PaperTrail::Version'),
+    concept: Field::BelongsTo,
+    id: Field::String.with_options(searchable: false),
+    source_table_name: Field::String,
+    source_attribute_name: Field::String,
+    additional_attributes: Field::String.with_options(searchable: false),
+    identifiability: Field::Number,
+    sensitivity: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    source_old_attribute_name: Field::String,
+    academic_year_collected_from: Field::Number,
+    academic_year_collected_to: Field::Number,
+    collection_terms: Field::String,
+    values: Field::Text,
+    description_en: Field::Text,
+    description_cy: Field::Text,
+    npd_alias: Field::String,
+    data_type: Field::String,
+    educational_phase: Field::String
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    source_table_name
+    source_attribute_name
+    concept
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    npd_alias
+    source_table_name
+    source_attribute_name
+    description_en
+    description_cy
+    concept
+    identifiability
+    sensitivity
+    source_old_attribute_name
+    academic_year_collected_from
+    academic_year_collected_to
+    collection_terms
+    values
+    data_type
+    educational_phase
+    created_at
+    updated_at
+    additional_attributes
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    npd_alias
+    source_table_name
+    source_attribute_name
+    identifiability
+    sensitivity
+    source_old_attribute_name
+    academic_year_collected_from
+    academic_year_collected_to
+    collection_terms
+    values
+    description_en
+    description_cy
+    data_type
+    educational_phase
+    concept
+    additional_attributes
+  ].freeze
+
+  # Overwrite this method to customize how data elements are displayed
+  # across all pages of the admin dashboard.
+  #
+  def display_resource(data_element)
+    "#{data_element.source_table_name}.#{data_element.source_attribute_name}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@
 
 Rails.application.routes.draw do
   devise_for :admin_users unless Rails.env.test?
+  namespace :admin do
+    resources :categories
+    resources :concepts
+    resources :data_elements, only: %i[index show]
+    resources :admin_users, only: %i[index show]
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+    root to: 'categories#index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '(/:locale)', locale: /en|cy/, defaults: { locale: 'en' } do
     resources :categories, only: %i[index show]
