548c85d632851a281e731e91dc68b65bd4fca313
cacquani <cacquani@mail.com>
2019-04-30 14:40:20 +0100

    #120 rails logic and views for nestable

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class CategoriesController < Admin::ApplicationController
+    helper NestableHelper
+
     # To customize the behavior of this controller,
     # you can overwrite any of the RESTful actions. For example:
     #
@@ -19,5 +21,40 @@ module Admin
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
     # for more information
+
+    def tree
+      @categories = Category.arrange(order: :position)
+    end
+
+    def sort
+      render plain: '' && return unless request.post? && params['tree_nodes'].present?
+
+      begin
+        update = -> { update_tree(params[:tree_nodes]) }
+
+        PgSearch.disable_multisearch do
+          ActiveRecord::Base.transaction { update.call }
+        end
+        PgSearch::Multisearch.rebuild(Category)
+
+        message = "<strong>#{I18n.t('admin.actions.nestable.success')}!</strong>"
+      rescue StandardError => e
+        message = "<strong>#{I18n.t('admin.actions.nestable.error')}</strong>: #{e}"
+      end
+
+      render plain: message
+    end
+
+  private
+
+    def update_tree(tree_nodes, parent_node = nil)
+      tree_nodes.each do |key, value|
+        model = Category.find(value['id'].to_s)
+        model.parent = parent_node || nil
+        model.position = key.to_i + 1
+        model.save!
+        update_tree(value['children'], model) if value.key?('children')
+      end
+    end
   end
 end
