e299d071fd760fc1ed90228f33c3d7391af61d12
cacquani <cacquani@mail.com>
2019-04-30 14:15:03 +0100

    #120 - add jQuery to webpack init

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 // const vue =  require('./loaders/vue')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+  })
+)
 
 // environment.loaders.append('vue', vue)
 module.exports = environment
