8c4d835fa53e80e14584b04cff3dc4b599d98d17
cacquani <cacquani@mail.com>
2019-05-03 12:13:11 +0100

    #143 admin layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,19 @@ class ApplicationController < ActionController::Base
                                password: Rails.application.credentials.dig(:basic_auth, :password) || '',
                                unless: -> { Rails.env.test? }
   before_action :set_locale
+  layout :layout_by_resource
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+private
+
+  def layout_by_resource
+    if devise_controller?
+      'admin/application'
+    else
+      'application'
+    end
   end
 end
