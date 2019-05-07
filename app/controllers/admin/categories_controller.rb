afba3f441d1e1a2374e174cfda8ff4d77ecf5acf
cacquani <cacquani@mail.com>
2019-05-07 11:20:17 +0100

    #142 fix search in admin for categories and concepts

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,12 +7,21 @@ module Admin
     # To customize the behavior of this controller,
     # you can overwrite any of the RESTful actions. For example:
     #
-    # def index
-    #   super
-    #   @resources = Category.
-    #     page(params[:page]).
-    #     per(10)
-    # end
+    def index
+      search_term = params[:search].to_s.strip
+      resources = find_resources(search_term)
+      resources = apply_resource_includes(resources)
+      resources = order.apply(resources)
+      resources = resources.page(params[:page]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources,
+        search_term: search_term,
+        page: page,
+        show_search_bar: show_search_bar?,
+      }
+    end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
@@ -55,6 +64,15 @@ module Admin
         model.save!
         update_tree(value['children'], model) if value.key?('children')
       end
+    end
+
+    def find_resources(search_term = nil)
+      return Category.all if search_term.blank?
+
+      Category.where(id: PgSearch.multisearch(search_term)
+                                 .where(searchable_type: 'Category')
+                                 .pluck(:searchable_id))
+              .includes(:translations, concepts: :translations)
     end
   end
 end
