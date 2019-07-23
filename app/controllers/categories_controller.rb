66e8ab0def478541ed5cce64a1e274406215908c
cacquani <cacquani@mail.com>
2019-07-23 11:28:07 +0100

    Dev #217: Page title for accessibility

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,12 +6,14 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.includes(:translations).roots
+    @title = t('site_title')
   end
 
   def show
     @category = Category.includes(:translations, concepts: %i[data_elements])
                         .find(params.require(:id))
 
+    @title = @category.name
     breadcrumbs_for(category_leaf: @category)
   end
 end
