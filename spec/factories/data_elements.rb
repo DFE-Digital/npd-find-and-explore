656ee7d4c0165d9dfba978a9d71bdd140f523c38
cacquani <cacquani@mail.com>
2019-08-01 15:28:42 +0100

    Fix #249: fix failing specs

--- spec/factories/data_elements.rb
+++ spec/factories/data_elements.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :data_element do
     sequence(:npd_alias)             { |n| "NPD_Alias[#{n}]" }
-    sequence(:source_table_name)     { |n| "#{Faker::Lorem.word.strip}#{n}" }
+    sequence(:source_table_name)     { %w[KS2_Exam KS2_Pupil KS3_Candidate KS3_Indicators KS3_Result KS4_Exam KS4_Pupil KS5_Exam KS5_Student].sample }
     sequence(:source_attribute_name) { |n| "#{Faker::Creature::Animal.name.strip}#{n}" }
     description_en                   { Faker::Lorem.sentence(3, false, 3) }
     source_old_attribute_name        { [Faker::Creature::Animal.name.strip] }
