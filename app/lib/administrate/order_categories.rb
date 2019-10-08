c0902c7c66d292e8efa6791897cbf2bf142539e6
cacquani <cacquani@mail.com>
2019-10-08 14:40:32 +0100

    Dev #262 sort by all column headers

--- app/lib/administrate/order_categories.rb
+++ app/lib/administrate/order_categories.rb
@@ -2,11 +2,35 @@
 
 module Administrate
   class OrderCategories < Administrate::Order
+    def apply(relation)
+      return order_by_association(relation) unless reflect_association(relation).nil?
+
+      order = "#{relation.table_name}.#{attribute} #{direction}"
+
+      return relation.reorder(order) if relation.columns_hash.keys.include?(attribute.to_s)
+
+      return reorder(relation) if should_reorder?(relation)
+
+      relation
+    end
+
+  private
+
+    def should_reorder?(relation)
+      attribute.present? &&
+        (relation.first.respond_to?(:translations) &&
+         relation.first.translations.first.respond_to?(attribute))
+    end
+
     def order_by_count(relation)
       relation
         .left_joins(attribute.to_sym)
         .group(:id, 'category_translations.name')
         .reorder("COUNT(#{attribute}.id) #{direction}")
+    end
+
+    def reorder(relation)
+      relation.reorder(attribute => direction)
     end
   end
 end
