e403f1eee413711184cf3df5efd8f0d810c6e191
cacquani <cacquani@mail.com>
2019-10-22 16:37:21 +0100

    Dev #401 more specs and fix indexing

--- spec/system/search_page_spec.rb
+++ spec/system/search_page_spec.rb
@@ -55,17 +55,73 @@ RSpec.describe 'Search pages', type: :system do
     expect(page).to have_text(DataElement.first.concept.description)
   end
 
-  it 'Will filter concepts by category' do
-    Concept.all.each_with_index { |concept, i| concept.update(name: "FSM #{i}") }
-    visit '/'
-    fill_in('search', with: 'FSM')
-    click_button('Search')
+  context 'Filter search results' do
+    before do
+      tabs = %w[Year7 KS2 KS3 KS4 KS5 CLA]
+      cla = [true, false]
+      Concept.all.each_with_index do |concept, i|
+        concept.data_elements.each_with_index do |de, j|
+          de.update(academic_year_collected_from: 1990 + j + (10 * i),
+                    academic_year_collected_to: 1995 + j + (10 * i),
+                    tab_name: tabs[(j * (i + 1)) % 6],
+                    is_cla: cla[i % 2])
+        end
+        concept.update(name: "FSM #{i}")
+      end
+    end
 
-    expect(page).to have_text('Showing all 2 results')
+    it 'Will filter concepts by category' do
+      visit '/'
+      fill_in('search', with: 'FSM')
+      click_button('Search')
 
-    check("category_id-#{Concept.first.category_id}", allow_label_click: true)
-    expect(page).to have_text('Displaying 1 result')
-    expect(page).to have_text(Concept.first.category.name.upcase)
-    expect(page).to have_text(Concept.first.description)
+      expect(page).to have_text('Showing all 2 results')
+
+      check("category_id-#{Concept.first.category_id}", allow_label_click: true)
+      expect(page).to have_text('Displaying 1 result')
+      expect(page).to have_text(Concept.first.category.name.upcase)
+      expect(page).to have_text(Concept.first.description)
+    end
+
+    it 'Will filter concepts by years' do
+      visit '/'
+      fill_in('search', with: 'FSM')
+      click_button('Search')
+      year = Concept.all.map(&:data_elements).flatten.map(&:academic_year_collected_from).min
+
+      expect(page).to have_text('Showing all 2 results')
+
+      check("years-#{year}", allow_label_click: true)
+      expect(page).to have_text('Displaying 1 result')
+    end
+
+    it 'Will filter concepts by tab names' do
+      visit '/'
+      fill_in('search', with: 'FSM')
+      click_button('Search')
+      concept = Concept.first
+      tabs = concept.data_elements.map(&:tab_name) - Concept.last.data_elements.map(&:tab_name)
+
+      expect(page).to have_text('Showing all 2 results')
+
+      check("tab_name-#{tabs.first}", allow_label_click: true)
+      expect(page).to have_text('Displaying 1 result')
+      expect(page).to have_text(concept.category.name.upcase)
+      expect(page).to have_text(concept.description)
+    end
+
+    it 'Will filter concepts by is_cla value' do
+      visit '/'
+      fill_in('search', with: 'FSM')
+      click_button('Search')
+      concept = Concept.first
+
+      expect(page).to have_text('Showing all 2 results')
+
+      check('is_cla-yes', allow_label_click: true)
+      expect(page).to have_text('Displaying 1 result')
+      expect(page).to have_text(concept.category.name.upcase)
+      expect(page).to have_text(concept.description)
+    end
   end
 end
