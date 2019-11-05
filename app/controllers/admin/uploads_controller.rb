6a6374a385ebe441e817b298d45325728ac6f026
cacquani <cacquani@mail.com>
2019-11-05 12:27:54 +0000

    Dev #420 keep last 5 failed uploads

--- app/controllers/admin/uploads_controller.rb
+++ app/controllers/admin/uploads_controller.rb
@@ -3,13 +3,23 @@
 module Admin
   class UploadsController < Admin::ApplicationController
     def index
-      DataTable::Upload.where.not(successful: true).destroy_all
-      DataTable::Upload.order(created_at: :desc).offset(5).destroy_all
-      InfArch::Upload.where.not(successful: true).destroy_all
-      InfArch::Upload.order(created_at: :desc).offset(5).destroy_all
+      trim_data_table_uploads
+      trim_inf_arch_uploads
 
       @data_table_uploads = DataTable::Upload.where(successful: true).order(created_at: :desc)
       @inf_arch_uploads = InfArch::Upload.where(successful: true).order(created_at: :desc)
+    end
+
+  private
+
+    def trim_data_table_uploads
+      DataTable::Upload.where.not(successful: true).offset(5).destroy_all
+      DataTable::Upload.order(created_at: :desc).offset(5).destroy_all
+    end
+
+    def trim_inf_arch_uploads
+      InfArch::Upload.where.not(successful: true).offset(5).destroy_all
+      InfArch::Upload.order(created_at: :desc).offset(5).destroy_all
     end
   end
 end
