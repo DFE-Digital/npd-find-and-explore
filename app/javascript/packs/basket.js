a48b032e84d7ecea3b5d37745737ad7c18b6f60e
cacquani <cacquani@mail.com>
2019-11-12 16:37:50 +0000

    Dev #427 add to list

--- app/javascript/packs/basket.js
+++ app/javascript/packs/basket.js
@@ -1,3 +1,29 @@
+function getElementsList() {
+  var list = localStorage.getItem('elementsList')
+  return (list ? JSON.parse(localStorage.getItem('elementsList')) : {})
+}
+
+function checkboxToLabel(element) {
+  element.parentElement.outerHTML = '<a href="" class="show-modal"><strong class="saved govuk-tag govuk-!-font-size-14">saved</strong></a>'
+}
+
+function addToMetadata(event) {
+  var elementsList = getElementsList()
+  document.querySelector('#save-to-metadata').setAttribute('disabled', true)
+
+  document.querySelectorAll('.basket-checkbox').forEach(function(element) {
+    if (element.checked) {
+      elementsList[element.dataset.id] = element.dataset
+      checkboxToLabel(element)
+    }
+  })
+  count = Object.keys(elementsList).length
+  document.querySelector('#npd-counter').innerText = count
+
+  localStorage.setItem('elementsList', JSON.stringify(elementsList))
+  localStorage.setItem('countElements', count)
+}
+
 function checkAll(event) {
   var checked = event.currentTarget.checked
   if (checked) {
@@ -18,19 +44,32 @@ function activateAdd(event) {
     enable = enable || list[i].checked
     if (list[i].checked) { count = count + 1 }
   }
+  enableSaveButton(enable, count, list.length)
+}
+
+function enableSaveButton(enable, count, length) {
   if (!enable) {
     document.querySelector('#data-element-all').checked = false
     document.querySelector('#save-to-metadata').setAttribute('disabled', true)
   } else {
     document.querySelector('#save-to-metadata').removeAttribute('disabled')
-    if (count === list.length)
+    if (count === length)
       document.querySelector('#data-element-all').checked = true
   }
 }
 
 $(document).ready(function() {
+  var selectedElements = Object.keys(getElementsList())
+
+  document.querySelector('#npd-counter').innerText = selectedElements.length
+
+  document.querySelector('#save-to-metadata').addEventListener('click', addToMetadata)
   document.querySelector('#data-element-all').addEventListener('change', checkAll)
   document.querySelectorAll('.basket-checkbox').forEach(function(element) {
-    element.addEventListener('change', activateAdd)
+    if (selectedElements.indexOf(element.dataset.id) > -1) {
+      checkboxToLabel(element)
+    } else {
+      element.addEventListener('change', activateAdd)
+    }
   })
 })
