fa786e5e54f46c0cb3ce17d66f094780480d7ba4
cacquani <cacquani@mail.com>
2019-11-22 16:52:56 +0000

    Dev #439 first draft

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class DatasetsController < ApplicationController
-  def show; end
+  def show
+    @dataset = Dataset
+               .includes(:translations, :data_elements)
+               .find(params.require(:id))
+
+    @title = @dataset.name
+    @description = @dataset.description
+  end
 end
