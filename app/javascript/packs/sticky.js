fcd16adb05a1ecee6d29a8c7e629ca91401dd446
cacquani <cacquani@mail.com>
2020-07-13 16:02:43 +0100

    IS08 - remove Add to List button
    * remove button
    * wire the checkboxes so that checking/unchecking will add/remove from
      the list

--- app/javascript/packs/sticky.js
+++ app/javascript/packs/sticky.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   var header = document.querySelector('#page-header')
   header.className = header.className + ' sticky-page-header'
 
-  var button = document.querySelector('#save-to-list-header')
-  button.className = button.className + ' sticky-save-to-list'
-
   var elementsHeader = document.querySelector('#data-elements-header')
   elementsHeader.className = elementsHeader.className + ' sticky-data-elements-header'
 })
