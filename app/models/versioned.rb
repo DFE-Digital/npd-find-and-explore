2d6c7b52c841ca45d5d7b5ee75523bb575575d68
cacquani <cacquani@mail.com>
2020-10-28 08:25:26 +0000

    Temporarily disable versioning

--- app/models/versioned.rb
+++ app/models/versioned.rb
@@ -4,5 +4,10 @@
 class Versioned < ApplicationRecord
   self.abstract_class = true
 
-  has_paper_trail
+  # Temporarily disable as the PO doesn't have immediate plans for this and it's
+  # only clogging the database.
+  # Re-enable when the PO decides to do something with it, or remove completely
+  # if they decide it's not needed.
+
+  # has_paper_trail
 end
