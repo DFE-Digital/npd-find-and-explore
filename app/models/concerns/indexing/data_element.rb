3c3c28b1d5f4f81a68b7778c2c6dc8bc24f0d96d
cacquani <cacquani@mail.com>
2020-11-07 11:29:21 +0000

    Dev #600 - remove dmetaphone

--- app/models/concerns/indexing/data_element.rb
+++ app/models/concerns/indexing/data_element.rb
@@ -19,16 +19,11 @@ module Indexing
                           dictionary: 'english',
                           tsvector_column: 'tsvector_content_tsearch',
                           prefix: true
-                        },
-                        dmetaphone: {
-                          dictionary: 'english',
-                          tsvector_column: 'dmetaphone_content_search',
-                          prefix: true
                         }
                       }
       scope :exact, ->(search_terms) {
         search_words = search_terms.split(/\s+/)
-        where(search_words.map { |word| "content_search LIKE #{connection.quote("%#{word}%")}" }.join(' AND '))
+        where(search_words.map { |word| "content_search LIKE #{connection.quote("% #{word} %")}" }.join(' AND '))
       }
 
       def update_pg_search_document
@@ -37,14 +32,12 @@ module Indexing
         conn.execute <<-SQL
           UPDATE data_elements
           SET
-            content_search = #{conn.quote([npd_alias, source_table_name, source_attribute_name,
+            content_search = #{conn.quote([npd_alias.split('_'), source_table_name, source_attribute_name,
                                            description, source_old_attribute_name].flatten.compact.join(' '))},
             tsvector_content_tsearch = setweight(to_tsvector(#{conn.quote(npd_alias || '')}), 'A') ||
                                        setweight(to_tsvector(#{conn.quote([source_table_name, source_attribute_name].compact.join(' '))}), 'B') ||
                                        setweight(to_tsvector(#{conn.quote(description || '')}), 'C') ||
                                        setweight(to_tsvector(#{conn.quote(source_old_attribute_name.compact.join(' '))}), 'D'),
-            dmetaphone_content_search = dmetaphone(#{conn.quote([npd_alias, source_table_name, source_attribute_name,
-                                                                 description, source_old_attribute_name].flatten.compact.join(' '))}),
             searchable_tab_names = #{conn.quote("{#{datasets.map(&:tab_name).compact.join(', ')}}")},
             updated_at = now()
         SQL
@@ -56,26 +49,23 @@ module Indexing
           SET
             content_search = tsvector_query.content,
             tsvector_content_tsearch = tsvector_query.tsvector_content,
-            dmetaphone_content_search = tsvector_query.dmetaphone_content,
             searchable_tab_names = tsvector_query.tab_names,
             updated_at = now()
 
           FROM (
             SELECT
               data_elements.id,
-              concat_ws(' ', data_elements.npd_alias, data_elements.source_table_name,
-                             data_elements.source_attribute_name, data_elements.description,
+              concat_ws(' ', replace(data_elements.npd_alias, '_', ' '),
+                             data_elements.source_table_name,
+                             data_elements.source_attribute_name,
+                             data_elements.description,
                              array_to_string(data_elements.source_old_attribute_name, ' ', ''))
                 AS content,
               setweight(to_tsvector(coalesce(data_elements.npd_alias, '')), 'A') ||
-                                       setweight(to_tsvector(coalesce(concat_ws(' ', data_elements.source_table_name, data_elements.source_attribute_name), '')), 'B') ||
-                                       setweight(to_tsvector(coalesce(data_elements.description, '')), 'C') ||
-                                       setweight(to_tsvector(array_to_string(data_elements.source_old_attribute_name, ' ', '')), 'D')
+              setweight(to_tsvector(coalesce(concat_ws(' ', data_elements.source_table_name, data_elements.source_attribute_name), '')), 'B') ||
+              setweight(to_tsvector(coalesce(data_elements.description, '')), 'C') ||
+              setweight(to_tsvector(array_to_string(data_elements.source_old_attribute_name, ' ', '')), 'D')
                 AS tsvector_content,
-              dmetaphone(concat_ws(' ', data_elements.npd_alias, data_elements.source_table_name,
-                                        data_elements.source_attribute_name, data_elements.description,
-                                        array_to_string(data_elements.source_old_attribute_name, ' ', '')))
-                AS dmetaphone_content,
               array_agg(DISTINCT(datasets.tab_name)) AS tab_names
 
             FROM data_elements
