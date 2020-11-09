5fcc0fedcf00ad0ae8b9c23d8efb5eeee4c846c9
cacquani <cacquani@mail.com>
2020-11-09 11:16:01 +0000

    Dev #599 - mail address from

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -50,7 +50,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@example.com'
+  config.mailer_sender = Rails.application.credentials.dig(Rails.env.to_sym, :office365, :user_name)
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
