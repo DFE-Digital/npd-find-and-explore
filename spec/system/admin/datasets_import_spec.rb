dbff39c7d8605c36aa2482d30a1032811783fe05
cacquani <cacquani@mail.com>
2020-12-13 21:42:17 +0000

    Dev #645 - additional spec and fix badge

--- spec/system/admin/datasets_import_spec.rb
+++ spec/system/admin/datasets_import_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe 'Datasets Import', type: :system do
 
   it 'Will upload recognised datasets from a file' do
     visit 'admin/import_datasets/import'
-    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'reduced_table.xlsx'))
+    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'manual_import_reduced_table.xlsx'))
     click_on('Continue')
 
     find_button('Import recognised datasets only', wait: 5)
@@ -59,20 +59,43 @@ RSpec.describe 'Datasets Import', type: :system do
     expect(page).to have_text('Import has been successful', wait: 5)
   end
 
-  xit 'Will upload unrecognised datasets from a file' do
-    # write some test for the new scenario
+  it 'Will upload unrecognised datasets from a file' do
+    visit 'admin/import_datasets/import'
+    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'manual_import_reduced_table.xlsx'))
+    click_on('Continue')
+
+    find_button('Continue to unrecognised datasets', wait: 5)
+    click_on('Continue to unrecognised datasets')
+
+    find_button('Continue', wait: 5)
+    find_all('[value="ignore"]', visible: :any).each do |element|
+      element.set(true)
+    end
+    id = DataTable::Tab.find_by(tab_name: 'SLD-KS2_PT_05-06_to_15-16').id
+    find("#data_tab_#{id}_action_create", visible: :all).set(true)
+    find("#data_tab_#{id}_new_dataset_name", visible: :all).fill_in(with: 'SLD-KS2_PT_05-06_to_15-16')
+    find("#data_tab_#{id}_new_dataset_desc", visible: :all).fill_in(with: 'SLD-KS2_PT_05-06_to_15-16')
+    find("#data_tab_#{id}_new_dataset_alias", visible: :all).fill_in(with: 'SLD Alias')
+
+    click_on('Continue')
+
+    find_button('Confirm', wait: 5)
+    find('#proceed-yes', visible: :all).set(true)
+    click_on('Confirm')
+
+    expect(page).to have_text('Import has been successful', wait: 5)
   end
 
   it 'Will have a Cancel import button' do
     visit 'admin/import_datasets/import'
-    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'reduced_table.xlsx'))
+    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'manual_import_reduced_table.xlsx'))
     click_on('Continue')
     expect(page).to have_button('Cancel import', wait: 5)
   end
 
   it 'Will abort the upload' do
     visit 'admin/import_datasets/import'
-    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'reduced_table.xlsx'))
+    attach_file('file-upload', Rails.root.join('spec', 'fixtures', 'files', 'manual_import_reduced_table.xlsx'))
     click_on('Continue')
     find_button('Cancel import', wait: 5)
     click_on('Cancel import')
