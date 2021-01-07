d10242f8ba9cb2e306063869c3ac3c72211f6884
cacquani <cacquani@mail.com>
2021-01-07 19:02:01 +0000

    Dev #676 - change root

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Rails.application.routes.draw do
     end
     resources :uploads, only: %i[index]
 
-    root to: 'categories#tree'
+    root to: 'admin_users#index'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
