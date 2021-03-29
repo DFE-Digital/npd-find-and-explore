7b2f08c3c925737c3db45ad0d1b43bc872f9b670
cacquani <cacquani@mail.com>
2021-03-29 18:51:50 +0100

    Dev #702 - datasets index

--- app/controllers/admin/datasets_controller.rb
+++ app/controllers/admin/datasets_controller.rb
@@ -2,6 +2,10 @@
 
 module Admin
   class DatasetsController < Admin::ApplicationController
+    layout :layout_by_resource
+
+    before_action :generate_breadcrumbs, only: %i[index]
+
     def destroy
       requested_resource.data_table_tabs.destroy_all
       requested_resource
@@ -15,6 +19,21 @@ module Admin
         flash[:error] = requested_resource.errors.full_messages.join('<br/>')
       end
       redirect_to action: :index
+    end
+
+  private
+
+    def layout_by_resource
+      if %w[index].include?(params[:action])
+        'admin/application'
+      else
+        'admin/side_menu'
+      end
+    end
+
+    def generate_breadcrumbs
+      custom_breadcrumbs_for(admin: true,
+                             leaf: params[:id].present? ? requested_resource.name : I18n.translate('admin.datasets.title'))
     end
   end
 end
