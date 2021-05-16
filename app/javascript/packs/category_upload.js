afc564b9cd4e3505edda14b0444e9666decafccf
cacquani <cacquani@mail.com>
2021-05-16 17:48:14 +0100

    Dev #740: Change categories import text
    * Change text
    * Fix some JS scripts

--- app/javascript/packs/category_upload.js
+++ app/javascript/packs/category_upload.js
@@ -1,3 +1,6 @@
+import '../src/loader.scss'
+
+import displayLoader from '../src/loader-display.js'
 import $ from 'jquery'
 import '../src/govuk-frontend.js'
 
@@ -6,8 +9,12 @@ $(document).on('ajax:success', function(event) {
   var data = detail[0], status = detail[1], xhr = detail[2]
 
   setTimeout(function() {
-    $('#govuk-box-container').hide();
+    $('#govuk-box-container').hide()
+    $('.main-content__header').show()
+    $('fieldset').show()
+    $('.govuk-breadcrumbs').show()
     document.querySelector('#form-group').outerHTML = detail[0].body.innerHTML
     document.getElementById('submit-upload').removeAttribute('disabled')
+    $('.loader-on-submit').submit(displayLoader)
   }, 500)
 })
--- app/javascript/packs/loader.js
+++ app/javascript/packs/loader.js
@@ -1,25 +1,9 @@
 import '../src/loader.scss'
-import '../src/loader.js'
+import displayLoader from '../src/loader-display.js'
 import $ from 'jquery'
 
 window.loader = new GOVUK.Loader()
 
 $(document).ready(function() {
-  $('.loader-on-submit').submit(function(event) {
-    if ($(event.currentTarget).attr('disabled')) {
-      return;
-    }
-
-    $(event.currentTarget).parents('.govuk-form-group').removeClass('govuk-form-group--error');
-    $(event.currentTarget).find('.govuk-error-message').hide();
-    $('button').attr('disabled', true);
-    $('#govuk-box-container').show();
-
-    window.loader.init({
-      container: 'govuk-box-message',
-      label: true,
-      labelText: '',
-      size: '175px',
-    });
-  });
+  $('.loader-on-submit').submit(displayLoader);
 });
